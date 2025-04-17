57af565d15c06f99b7b4bb84683f1d72ce8337ac
Zeet Geek <zeetgeek435@gmail.com>
2025-04-17 16:30:51 +0530

    set data in localstorage

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -6,8 +6,23 @@ const count = document.querySelector<HTMLSpanElement>('#count')!;
 const total = document.querySelector<HTMLSpanElement>('#total')!;
 const movieSelect = document.querySelector<HTMLSelectElement>('#movie-select')!;
 
+// consent
+const SEATS_STORAGE_KEY = 'selected-seats';
+const MOVIE_STORAGE_KEY = 'selected-movie';
+const PRICE_STORAGE_KEY = 'selected-price';
+
+let ticketPrice: number;
+let movieIndex: number;
+
 movieSelect.addEventListener('change', () => {
+    ticketPrice = parseInt(movieSelect?.value?.trim());
+    movieIndex = movieSelect.selectedIndex;
+
+    localStorage.setItem(MOVIE_STORAGE_KEY, JSON.stringify(movieIndex));
+    localStorage.setItem(PRICE_STORAGE_KEY, JSON.stringify(ticketPrice));
+
     countSelectedSeats();
+    displayData();
 });
 
 allSeats.addEventListener('click', (e) => {
@@ -16,13 +31,37 @@ allSeats.addEventListener('click', (e) => {
         element.classList.toggle('selected');
         countSelectedSeats();
     }
+    displayData();
 });
 
 function countSelectedSeats() {
     const selectedSeats = allSeats.querySelectorAll<HTMLDivElement>('.seat.selected');
+    const selectedSeatIndex = [...selectedSeats].map((item) => [...seats].indexOf(item));
+    localStorage.setItem(SEATS_STORAGE_KEY, JSON.stringify(selectedSeatIndex));
+    displayData();
+}
+
+function displayData() {
+    const selectedSeats = JSON.parse(localStorage.getItem(SEATS_STORAGE_KEY) || '[]');
+    const selectedMovie = JSON.parse(localStorage.getItem(MOVIE_STORAGE_KEY) || '');
+    const ticketPrice = JSON.parse(localStorage.getItem(PRICE_STORAGE_KEY) || '');
+
     if (selectedSeats) {
-        const seatLength = selectedSeats.length;
-        count.innerText = seatLength.toString();
-        total.innerText = `$${seatLength * parseInt(movieSelect.value)}`;
+        seats.forEach((item, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                item.classList.add('selected');
+            }
+        });
+        count.innerText = `${selectedSeats.length}`;
+    }
+
+    if (selectedMovie) {
+        movieSelect.selectedIndex = selectedMovie;
+    }
+
+    if (ticketPrice) {
+        total.innerText = `$${selectedSeats.length * ticketPrice}`;
     }
 }
+
+displayData();
